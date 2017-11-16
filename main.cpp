e8b218171215c5e6e995066c246c068c66348a2b
Спеле Владимир <spele.vova@gmail.com>
2017-11-16 21:05:36 +0500

    Убрал тесты в Main, добавил переменные в Solver

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <armadillo>
 
-using namespace arma;
 
 int main(int argc, char *argv[])
 {
@@ -10,12 +8,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    mat A = randu<mat>(100, 100);
-    mat B = randu<mat>(100, 100);
-    A(0,0) = 5;
-    mat result = A * B.t();
-    result(0, 0) = 0;
-    result.save("result.csv", csv_ascii);
-    std::cout << result << std::endl;
     return a.exec();
 }
--- solver.h
+++ solver.h
@@ -1,6 +1,10 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
+#include <armadillo>
+
+using namespace arma;
+
 /*!
  \class Solver
  \brief Класс, в котором будут происходить вычисления
@@ -9,6 +13,11 @@ class Solver
 {
 public:
     Solver();
+    Mat<float> H;
+    Mat<float> Kx, Ky;
+    Mat<float> Sw;
+    Col<float> Krw;
+
 };
 
 #endif // SOLVER_H
