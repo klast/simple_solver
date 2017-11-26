03fb96b36e8692bf92b657e8486aa0230b480732
Кашаева Эльвина <elvincka@mail.ru>
2017-11-26 20:41:45 +0500

    Новые поправки

--- reader.cpp
+++ reader.cpp
@@ -67,7 +67,7 @@ void Reader::read()
 void Reader::read_1d_array(QString keyword_name)
 {
     QVector<float> this_array;
-    QString tmp; float p;
+    QString tmp, s, s1; float p, pp; int i; p=0;
     while(true)
     {
         tmp = datafile.readLine();
@@ -76,11 +76,27 @@ void Reader::read_1d_array(QString keyword_name)
             QStringList my_row = tmp.split(' ');
             for(QString item: my_row)
             {
-                p = item.toFloat();
-                this_array.push_back(p);
-            }
-        }
-    }
+                if (!tmp.contains("*") ){
+                    p = item.toFloat();
+                    this_array.push_back(p);
+                }
+                else {
+                    QStringList my_row = item.split('*');
+                      for(QString item: my_row){
+                       s.append(item+" ");}
+                       s = s.prepend(" ");
+                       s1 = s.section(' ', 1, 1);
+                        p = s1.toFloat();
+                        s1 = s.section(' ', 2, 2);
+                        pp = s1.toFloat();
+                        for (i = int(p); i!=0; i--){
+                        this_array.push_back(pp);
+                        }s.clear();
+                       }
+                      }
+                     }
+                    }
+
     qDebug() << this_array;
     input_1d_arrays[keyword_name] = this_array;
 }
